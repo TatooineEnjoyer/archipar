9eea411ae770eff810d89cc7fd8a4df6f52872d9
TatooineEnjoyer <dylanisthebob@outlook.com>
2026-05-21 15:56:49 +0300

    Create page.tsx

---
+++ src/app/bath-complexes/page.tsx
@@ -0,0 +1,6 @@
+import ProductPage from "@/components/ProductPage";
+import { products } from "@/data/products";
+
+export default function BathComplexesPage() {
+  return <ProductPage product={products["bath-complexes"]} />;
+}
