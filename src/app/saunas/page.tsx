8f424a46358e641b7c232f55821fd1dbe1d52cdd
TatooineEnjoyer <dylanisthebob@outlook.com>
2026-05-21 15:57:29 +0300

    Create page.tsx

---
+++ src/app/saunas/page.tsx
@@ -0,0 +1,6 @@
+import ProductPage from "@/components/ProductPage";
+import { products } from "@/data/products";
+
+export default function SaunasPage() {
+  return <ProductPage product={products.saunas} />;
+}
