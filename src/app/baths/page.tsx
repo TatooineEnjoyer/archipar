88062586ff447ac54a5a4187035c67aca8aa091a
TatooineEnjoyer <dylanisthebob@outlook.com>
2026-05-21 15:57:10 +0300

    Create page.tsx

---
+++ src/app/baths/page.tsx
@@ -0,0 +1,6 @@
+import ProductPage from "@/components/ProductPage";
+import { products } from "@/data/products";
+
+export default function BathsPage() {
+  return <ProductPage product={products.baths} />;
+}
