c7e809db767a20d28792500e9f829167506b9a63
TatooineEnjoyer <dylanisthebob@outlook.com>
2026-05-21 15:57:48 +0300

    Create page.tsx

---
+++ src/app/hamams/page.tsx
@@ -0,0 +1,6 @@
+import ProductPage from "@/components/ProductPage";
+import { products } from "@/data/products";
+
+export default function HamamsPage() {
+  return <ProductPage product={products.hamams} />;
+}
